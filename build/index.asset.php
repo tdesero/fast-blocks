2a4aca322df1913a4b0acaabb8b8264eda5b2743
Thomas Deser <thomas.deser@neueformation.com>
2021-05-30 15:21:39 +0200

    added a fallback for wp content

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'c90191bd21d9339f1293b41aacefa541');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '457af4078dc4812396cad50c2307ab09');
