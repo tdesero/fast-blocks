5d211d6b19f5c62264c724cc8ed2b9d939dab4ec
Thomas Deser <thomas.deser@neueformation.com>
2021-06-02 21:02:59 +0200

    delete old comment

--- fast-blocks.php
+++ fast-blocks.php
@@ -13,14 +13,6 @@
  * @package           create-block
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
- */
-
 class SingleFastBlock {
 	public $settings;
 
