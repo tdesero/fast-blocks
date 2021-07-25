bb952d85a46073170aa5c7a8577a8f9605d2163e
Thomas Deser <thomas.deser@neueformation.com>
2021-07-25 11:55:12 +0200

    editable inspector fields for popover editView

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-server-side-render'), 'version' => 'f5021608b73282c1921c1765ea84d054');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ad380f6a134060c36553114cac4fad80');
--- src/components/EditorPopover.js
+++ src/components/EditorPopover.js
@@ -1,11 +1,10 @@
-import { Card, CardBody, CardHeader, Popover, Button, Modal } from '@wordpress/components';
+import { Button, Modal } from '@wordpress/components';
 import { FieldControl } from './FieldControl';
 import { useState } from '@wordpress/element';
 import { edit } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n'; 
 
 export function EditorPopover( { title, fields, editProps, isSelected } ) {
-	/* only for popover */
 	const [ popoverVisible, setPopoverVisible ] = useState( false );
 	const hidePopover = () => {
 		setPopoverVisible( () => false );
@@ -13,15 +12,15 @@ export function EditorPopover( { title, fields, editProps, isSelected } ) {
 	const showPopover = () => {
 		setPopoverVisible( () => true );
 	};
-
 	const isSelectedClass = isSelected ? ' is-selected' : '';
 	
 	return (
 		<div className={"fbl_edit-btn-overlay" + isSelectedClass}>
-			<Button isPrimary icon={edit} onClick={showPopover} />
-			{ popoverVisible && (
-				<Modal title={'Block: '+ title} onRequestClose={ hidePopover } style={{ width: 600 }} shouldCloseOnClickOutside={false}>
-					{ Object.entries( fields ).map(
+			{ Object.entries( fields )
+					.filter( ([ fieldName, field ]) => {
+						return field.location === 'inspector';
+					} )
+					.map(
 						( [ fieldName, field ] ) => {
 							const props = {
 								editProps,
@@ -31,7 +30,26 @@ export function EditorPopover( { title, fields, editProps, isSelected } ) {
 							return <FieldControl { ...props } />;
 						}
 					) }
-					<Button isPrimary onClick={hidePopover}>Okay</Button>
+			<Button isPrimary icon={edit} onClick={showPopover} />
+			{ popoverVisible && (
+				<Modal title={'Block: ' + title} onRequestClose={ hidePopover } style={{ width: 600 }} shouldCloseOnClickOutside={false}>
+					{ Object.entries( fields )
+							.filter( ([ fieldName, field ]) => {
+								return field.location !== 'inspector';
+							} )
+							.map(
+								( [ fieldName, field ] ) => {
+									const props = {
+										editProps,
+										fieldName,
+										field
+									}
+									return <FieldControl { ...props } />;
+								}
+							) }
+					<div style={{ textAlign: 'right', marginTop: '2rem' }}>
+						<Button isPrimary onClick={hidePopover}>Okay</Button>
+					</div>
 				</Modal>
 			)}
 		</div>
