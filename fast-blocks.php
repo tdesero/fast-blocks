bdb3946c3cf149e89af0152b9cb18fdf0b394725
Thomas Deser <thomas.deser@neueformation.com>
2021-07-27 12:26:24 +0200

    v0.7.5

--- fast-blocks.php
+++ fast-blocks.php
@@ -4,7 +4,7 @@
  * Description:       Create Custom Gutenberg Blocks fast and easy with PHP only.
  * Requires at least: 5.7
  * Requires PHP:      7.0
- * Version:           0.7.0
+ * Version:           0.7.5
  * Author:            Tom D
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
