a20ae0aed5f3aad703e52d79ee5903881e52a742
tdesero <tom.deser@googlemail.com>
2024-07-28 22:38:30 +0200

    chore: update version

--- fast-blocks.php
+++ fast-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Create Custom Gutenberg Blocks fast and easily in PHP.
  * Requires at least: 5.7
  * Requires PHP:      7.0
- * Version:           0.9.0
+ * Version:           0.9.1
  * Author:            Tom D
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      tdesero
 Tags:              block
 Tested up to:      6.6
-Stable tag:        0.9.0
+Stable tag:        0.9.1
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
