0638acae9da6d47b889e33f02ba1a4c01b07cf6d
tdesero <tom.deser@googlemail.com>
2024-08-05 19:12:04 +0200

    feat: disable auto updates for upcoming versions

--- fast-blocks.php
+++ fast-blocks.php
@@ -53,3 +53,24 @@ function fast_blocks_view_instance()
  * Helpers (for theme developers)
  */
 require_once(__DIR__ . '/inc/helpers.php');
+
+/**
+ * Remove automatic updates
+ */
+function fast_blocks_disable_auto_update($update, $item)
+{
+	if ($item->slug === 'fast-blocks') {
+		// Get the current version of the installed plugin
+		$plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/' . $item->slug . '/' . $item->slug . '.php');
+		$current_version = $plugin_data['Version'];
+
+		// Specify the versions you want to disable auto-updates for
+		$versions_to_disable = ['0.9.1', '0.9.2', '0.9.3', '0.9.4', '0.9.5', '0.9.6', '0.9.7', '0.9.8', '0.9.9'];
+
+		if (in_array($current_version, $versions_to_disable)) {
+			return false; // Disable automatic updates for these specific versions
+		}
+	}
+	return $update; // For other plugins, leave the setting unchanged
+}
+add_filter('auto_update_plugin', 'fast_blocks_disable_auto_update', 10, 2);
